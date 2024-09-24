2fcb032c942101b9ef032a99c5f6283bcf525c15
Alberto <ahernandezlopez@safareyes.es>
2024-09-24 18:01:46 +0200

    Clases Linea_bebida y Linea_plato

---
+++ src/main/java/com/example/restaurante/Linea_bebida.java
@@ -0,0 +1,36 @@
+package com.example.restaurante;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate; // Import the LocalDate class
+
+@Entity
+@Table(name = "linea_bebida",schema = "Sabores_de_casa", catalog = "postgres")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Linea_bebida {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "cantidad", nullable = false)
+    private Integer cantidad;
+
+    @Column(name = "precio")
+    private Double precio;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_bebida", nullable = false)
+    private Bebida idBebida;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_pedido", nullable = false)
+    private Pedido idPedido;
+
+}
---
+++ src/main/java/com/example/restaurante/Linea_plato.java
@@ -0,0 +1,40 @@
+package com.example.restaurante;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate; // Import the LocalDate class
+
+@Entity
+@Table(name = "linea_plato",schema = "Sabores_de_casa", catalog = "postgres")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Linea_plato {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "cantidad", nullable = false)
+    private Integer cantidad;
+
+    @Column(name = "precio")
+    private Double precio;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_plato", nullable = false)
+    private Plato idPlato;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_pedido", nullable = false)
+    private Pedido idPedido;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_tipo_plato", nullable = false)
+    private Tipo_plato idTipoPlato;
+
+}
